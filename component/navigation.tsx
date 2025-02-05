5f68fa9ab3eda26dd70f008ff13aa209f75e8d0b
jwpwp <jw03287@naver.com>
2025-02-06 04:39:55 +0900

    Feat: usePathname 훅을 사용. 'use client' 문장도 추가.

--- component/navigation.tsx
+++ component/navigation.tsx
@@ -1,14 +1,19 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const path = usePathname();
   return (
     <nav>
       <ul>
         <li>
           <Link href="/">Home</Link>
+          {path === "/" ? "🔥" : ""}
         </li>
         <li>
           <Link href="child1">Child1</Link>
+          {path === "/child1" ? "🔥" : ""}
         </li>
       </ul>
     </nav>
