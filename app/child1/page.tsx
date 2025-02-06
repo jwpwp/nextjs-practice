ef28b29200cae8663f1860e2ef7146c943aed7d1
jwpwp <jw03287@naver.com>
2025-02-07 03:02:48 +0900

    Feat: 불필요한 반복을 줄이기 위해 layout 컴포넌트를 활용해서 navigation 컴포넌트 배치.

--- app/child1/page.tsx
+++ app/child1/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "../../component/navigation";
 export default function Child1() {
   return (
     <div>
-      <Navigation />
       <h1>Child1</h1>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "../component/navigation";
 export default function Hello() {
   return (
     <div>
-      <Navigation />
       <h1>HELLO</h1>
     </div>
   );
