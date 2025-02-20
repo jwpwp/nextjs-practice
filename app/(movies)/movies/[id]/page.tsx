c11e7588d3e5639d5aa125bba17f30b8e796e10b
jwpwp <jw03287@naver.com>
2025-02-21 04:36:33 +0900

    Feat: movie의 이름을 title로 하는 기능 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,20 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../component/movie-info";
+import MovieInfo, { getMovie } from "../../../../component/movie-info";
 import MovieVideos from "../../../../component/movie-videos";
 
-export default async function MovieDetail({
-  params,
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params }: IParams) {
+  const { id } = await params;
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params }: IParams) {
   const { id } = await params;
 
   // Promise.all()로 묶어서 MovieInfo와 MovieVideos를 불러올 수도 있지만 이렇게 하면 두 작업이 모두 끝나야 한번에 나옴.(그리고 이 경우에는 return 윗 부분에서 fetch하기때문에 loading.tsx가 작동함.)
